784516ca6dcd3fd14a6f5a694a46adf6f8bcdc46
Rafael <rafael.yano59@gmail.com>
2019-02-27 15:12:39 +0000

    home almost

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -156,6 +156,16 @@
           }
         }); 
         
+        $("a[href^='#']").click(function(e) {
+        	e.preventDefault();
+        	
+        	var position = $($(this).attr("href")).offset().top;
+        
+        	$("body, html").animate({
+        		scrollTop: position
+        	}, 500 );
+        });
+        
         
     });
     
