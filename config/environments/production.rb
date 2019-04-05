6e478039f603e711904fcc15330a7432585e75b3
Rafael <rafael.yano59@gmail.com>
2019-04-05 19:18:19 +0000

    contato finalizado

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,10 +95,10 @@ end
 
 ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
-    :port => ’587′,
+    :port => '587',
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => ‘heroku.com’
+    :domain => 'heroku.com'
 }
 ActionMailer::Base.delivery_method = :smtp
